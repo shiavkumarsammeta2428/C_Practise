defff34cfb87ad7b6547a8f4b37a26d8d7568500
shiva <shivakumarsammeta2428@gmail.com>
2025-04-02 22:13:21 +0530

    2D-Array

--- c_basic/hello.c
+++ c_basic/hello.c
@@ -5,9 +5,30 @@
 
 void main(){
 	printf("Hello_world\n");
-
-
 	printf("Second_from_code\n\n");
+
+	int tarr[5][3];
+
+	for(int i=0;i<5;i++){
+
+		for(int j=0;j<3;j++){
+
+			printf("\nIntput the index arr[%d][%d]:",i,j);
+			scanf("%d",&tarr[i][j]);
+		}
+
+	}
+
+	printf("The output \n");
+
+	for(int i=0;i<5;i++){
+
+		for(int j=0;j<3;j++){
+
+			printf("%d ",tarr[i][j]);
+		}
+		printf("\n");
+	}
 
 
 }
